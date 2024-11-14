db25afe63a70870621bde70d36126991c7173113
mani06001 <mani06001@E-PW05C37P>
2024-11-14 12:33:40 +0100

    Add project files.

---
+++ Uppgift 13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console .ReadLine());
+            if (ålder < 16)
+
+            {
+                Console.WriteLine("du är för ung");
+            }
+
+            else if (ålder > 19)
+            {
+                Console.WriteLine("du är för gammal");
+            }
+
+            else
+            {
+                Console.WriteLine("du är i rätt ålder");
+            }
+        }
+    }
+}
